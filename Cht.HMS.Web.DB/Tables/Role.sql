d597be1dd5b1d4e029f72c3da57d1ef22895ba0b
Durga Prasad Bgahuthu <dbaghuthu@betalen.in>
2025-01-30 10:21:07 +0530

    creating and setting up db with role table and seed script

---
+++ Cht.HMS.Web.DB/Tables/Role.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Role]
+(
+	[RoleId]                        uniqueidentifier       NOT NULL    PRIMARY KEY   Default     newid(),
+	[Name]                          varchar(max)           NULL,
+	[Code]                          varchar(max)           NULL,
+	[CreatedBy]                     uniqueidentifier       NULL,
+	[CreatedOn]                     datetimeoffset         NULL,
+	[ModifiedBy]                    uniqueidentifier       NULL,
+	[ModifiedOn]                    datetimeoffset         NULL,
+	[IsActive]                      bit                    NULL
+)
